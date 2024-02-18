616dcb014b17d09171ee34bf12bcab1e0ed8c892
chengalapattu vivek sai <117505203+vickycode674@users.noreply.github.com>
2024-02-18 17:09:14 +0530

    Add files via upload

---
+++ QrCode_Generator/QrGenrator.py
@@ -0,0 +1,3 @@
+import qrcode as qr #alias nick name
+img = qr.make("https://www.linkedin.com/in/chengalapattu-vivek-sai/")
+img.save("Vivek_LinkedIn.png")
---
+++ QrCode_Generator/qr_2.py
@@ -0,0 +1,11 @@
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(version=1,
+error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=4,)
+
+qr.add_data("https://www.linkedin.com/in/chengalapattu-vivek-sai/")
+qr.make(fit=True)
+
+img=qr.make_image(fill_color="red",back_color="aqua")
+img.save("linkedin.png")
