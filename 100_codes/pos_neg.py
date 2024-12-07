fad7cfa66eba62425f7bd2a0104d07b9aef29a8c
Chengalapattu Vivek Sai <chvivek674@gmail.com>
2024-12-07 17:01:22 +0530

    updated

--- 100_codes/pos_neg.py
+++ 100_codes/pos_neg.py
@@ -1,1 +1,42 @@
 #we have to take input value from the user
+#1  finding out positive or negative number
+# value=int(input("Enter a number to verify positive or negative")) #to take a input of integer
+
+# if value > 0:
+#   print("positive")
+
+# elif value==0:
+#   print("zero")
+
+# else :
+#   print("negative")
+
+# #2 Finding even or odd number 
+# value=int(input("Enter a number to verify positive or negative")) #to take a input of integer
+
+# if value%2==0:
+#     print("Even number")
+
+# else:
+#     print("Odd number") 
+
+#3 sum of n natuarl numbers
+# value=int(input("Enter a number to verify positive or negative")) #to take a input of integer
+
+# num = 0
+#we have to take for loop in the python
+# for i in range(1,value+1): #method of using for loop
+#     num+=i
+
+# print(num)
+
+#4 show n natural numbers
+
+# for i in range(1,value+1):
+#     print(i)
+
+#5 show n natural numbers with an particular interval values..
+
+
+
+
