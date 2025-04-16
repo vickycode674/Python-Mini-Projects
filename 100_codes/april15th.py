7aeab81abf5b909dc7bd9fef477e5238e6aca6d8
Chengalapattu Vivek Sai <chvivek674@gmail.com>
2025-04-16 14:50:05 +0530

    april 16th code

--- 100_codes/april15th.py
+++ 100_codes/april15th.py
@@ -41,7 +41,7 @@ def factors(n):
     facto = []
     for i in range(1,n+1):
         if n%i==0:
-            facto.append(i);
+            facto.append(i)
     return facto
     
 
@@ -58,6 +58,58 @@ def factors(n):
     return sorted(facto)
 
 print("Here are the factors of the number:", factors(10))
+
+# april 16th
+#finding prime factors of a number
+
+var = int(input("Enter a number to get is prime factors of it"))
+factors = []
+
+def is_prime(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i ==0:
+            return False
+    return True
+
+def prime_factor(n):
+    for i in range(2,n):
+        if n%i==0 and is_prime(i):
+            factors.append(i)
+    return factors
+    
+print(f"Here i the prime factors of {var} is========",prime_factor(var))
+
+#Strong number: A Strong Number is a number where the sum of the factorials of its 
+# digits equals the original number itself.
+
+# strong number 
+
+def facto(n):
+    total=1
+    while n>1:
+        total=total*n
+        n=n-1
+    return total
+    
+    
+def strong_number(n):
+    temp=n
+    value=0
+    while temp>0:
+        digit = temp%10
+        value = value + facto(digit)
+        temp=temp//10
+    
+    if n==value:
+        print("Yes yes what you said is correct this is strong number",n,value)
+    else:
+        print("No No Its not a strong strong_number",n,value)
+
+strong_number(145)
+
+       
 
     
    
